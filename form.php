79e61e0efaeb0474c91a8ccbc4253f465fe925aa
Send-Gilad-A-Message <99569463+Send-Gilad-A-Message@users.noreply.github.com>
2022-02-16 16:57:44 -0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,25 @@
+
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+if ($_GET["password"] != 'answer0821'){
+  echo '<script>alert("Wrong password, please try again...")</script>';
+} else {
+
+$connection = new AMQPStreamConnection('amqps://btmtimzd:kTi9RE4Rly6MCjm4NK0vJKv6Y7dQGQ7o@clam.rmq.cloudamqp.com/btmtimzd', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('to_gilad', false, false, false, false);
+
+$msg = new AMQPMessage($_GET["message");
+$channel->basic_publish($msg, '', 'to_gilad');
+
+echo " [x] Sent \n";
+
+$channel->close();
+$connection->close();
+?>
+}
